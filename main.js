84cfa3865cafd2506aa9ed05b52715473a380124
Julie <juliy0214561@gmail.com>
2023-08-23 15:12:24 +0800

    [update] 更新header、新增bootstrap5、bootstrina-icons、新增共用變數(顏色變數)

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 import './assets/scss/all.scss';
+import 'bootstrap/dist/js/bootstrap.min.js';
+// 載入bootstrap-icons
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 console.log("Hello world!");
